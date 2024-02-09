86c41ec477ce676a3556071db1233c7f0455252f
laurendolagofreitasb <laurendolagofreitasb@LAURENDOLAG6355>
2024-02-09 16:12:04 -0700

    Add project files.

---
+++ MyFirstASPNETWebAppFun/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyFirstASPNETWebAppFun.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ MyFirstASPNETWebAppFun/wwwroot/js/calculator.js
@@ -0,0 +1,19 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$(document).ready(function () {
+    $('#calculateTotal').click(function () {
+        var hours = parseFloat($('#hours').val());
+        var hourlyRate = parseFloat($('#hourlyRate').val());
+
+        if (isNaN(hours) || hours <= 0) {
+            $('.error').text('Please enter a valid number of hours.');
+            $('#total').text('');
+        } else {
+            $('.error').text('');
+            var total = hours * hourlyRate;
+            $('#total').text('$' + total.toFixed(2));
+        }
+    });
+});
